e2a8f2beac421cd6e457b98e0da9765551308dff
ornab <hzamil17@gmail.com>
2017-05-14 11:00:14 +0600

    Modified

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function roles(){
+        
+        return $this->belongsToMany('App\Role');
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
+use App\User;
+use App\Role;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/create', function(){
+    
+    $user = User::findOrFail(1);
+    
+    
+    $role = new Role(['name' => 'Subscriber' ]);
+    
+    $user->roles()->save($role);
+    
+    
+});
+
+
+Route::get('/read', function(){
+    
+    $user = User::findOrFail(1);
+    
+    foreach($user->roles as $role){
+        
+       echo $role->name . '<br>';
+        
+    }
+    
+    
 });
