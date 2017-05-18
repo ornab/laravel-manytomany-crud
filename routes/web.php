cbd214a7ca16017e3b2c89ee6a0cee8848dfefa8
ornab <hzamil17@gmail.com>
2017-05-18 11:01:03 +0600

    modified

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/create', function(){
     $user = User::findOrFail(1);
     
     
-    $role = new Role(['name' => 'Subscriber' ]);
+    $role = new Role(['name' => 'Author' ]);
     
     $user->roles()->save($role);
     
@@ -86,9 +86,40 @@ Route::get('/delete', function(){
     
 });
 
+// It will create new record in DB even if that record already exists
 
+Route::get('/attach', function(){
+    
+    $user = User::findOrFail(1);
+    
+    
+    $user->roles()->attach(3);
+    
+    
+    
+});
 
+// It will remove all the record of the specific query, in this case we detached role 3 from user 1 which we have attached by //the attach method
 
+Route::get('/detach', function(){
+    
+    $user = User::findOrFail(1);
+    
+    
+    $user->roles()->detach(3);
+    
+    
+});
+
+//Hace to do some study on this sync method
+
+Route::get('/sync', function(){
+    
+    $user = User::findOrFail(1);
+    
+    $user->roles()->sync([3,4]);
+    
+});
 
 
 
